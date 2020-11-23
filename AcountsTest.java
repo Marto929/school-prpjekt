127ef7856dcf41723c955ee0bf890fa87d4c93fb
Marto929 <72599446+Marto929@users.noreply.github.com>
2020-11-23 12:55:24 +0200

    Add files via upload

---
+++ AcountsTest.java
@@ -0,0 +1,14 @@
+package domashnoit;
+
+public class AcountsTest {
+
+
+		public static void main(String[] args) {
+			SavingsAccount marti = new SavingsAccount();
+			marti.deposit(1000);
+			marti.withdraw(200);
+
+		}
+
+	}
+}
---
+++ Student.java
@@ -0,0 +1,33 @@
+package domashnoit;
+public class Student {
+	private String name;
+	private double grade;
+	
+	public Student() {
+		
+	}
+	
+	public Student (String name, double grade) {
+		this.setName(name);
+		this.setGrade(grade);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getGrade() {
+		return grade;
+	}
+
+	public void setGrade(double grade) {
+		this.grade = grade;
+	}
+}
+
+
+
